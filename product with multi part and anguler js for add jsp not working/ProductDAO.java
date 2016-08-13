931166046b32cfeb589c6bfe9db5401dec187a47
sai9000 <saibhogaraju@gmail.com>
2016-08-13 08:10:49 +0530

    Add files via upload

---
+++ product with multi part and anguler js for add jsp not working/ProductDAO.java
@@ -0,0 +1,17 @@
+package com.watchstreets.dao;
+
+import java.util.List;
+
+import com.watchstreets.model.Product;
+
+
+public interface ProductDAO {
+	public void saveOrUpdate(Product product);
+		 
+	public Product get(int id);
+	public Product getProductByName(String name) ;
+
+	public void delete(int id) ;
+	public List<Product> list();
+
+}
---
+++ product with multi part and anguler js for add jsp not working/ProductDAOImpl.java
@@ -0,0 +1,91 @@
+package com.watchstreets.daoimpl;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.watchstreets.dao.ProductDAO;
+import com.watchstreets.model.Product;
+import com.watchstreets.model.User;
+
+
+@Repository("productDAO")
+public class ProductDAOImpl implements ProductDAO {
+
+	@Autowired
+	private SessionFactory sessionfactory;
+	public ProductDAOImpl(SessionFactory sessionfactory){
+		this.sessionfactory=sessionfactory;
+	}
+		
+		@Transactional
+		public void saveOrUpdate(Product product){
+			Transaction t=sessionfactory.getCurrentSession().beginTransaction();
+			
+			sessionfactory.getCurrentSession().saveOrUpdate(product);
+		t.commit();
+		
+		}
+		
+	    @Transactional
+		public Product get(int id) {
+			String hql = "from Product where id=" + "'"+ id +"'";
+			//  from supplier where id = '101'
+			Query query =  sessionfactory.getCurrentSession().createQuery(hql);
+			@SuppressWarnings("unchecked")
+			List<Product> listproduct = (List<Product>) query.list();
+			
+			if (listproduct != null && !listproduct.isEmpty()) {
+				return listproduct.get(0);
+			}
+			return null;
+		}
+	    @Transactional
+		public Product getProductByName(String name) {
+			// TODO Auto-generated method stub
+			
+				System.out.println("getting data in dao based on name");
+				Session session=this.sessionfactory.getCurrentSession();
+				Product product=(Product) session.get(Product.class,name);
+				System.out.println("data of user by name="+product);
+				return product;	
+
+		}
+		
+		@Transactional
+		public void delete(int id) {
+			Product product=new Product();
+			product.setId(id);
+			sessionfactory.getCurrentSession().delete(product);
+			
+		
+		}
+			
+		
+		@Transactional
+			public List<Product> list() {
+				@SuppressWarnings("unchecked")
+				List<Product> listProduct = (List<Product>) 
+				          sessionfactory.getCurrentSession()
+						.createCriteria(Product.class)
+						.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+				return listProduct;
+			}
+			
+
+			
+
+			
+	
+
+
+
+	
+}
